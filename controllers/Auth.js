46bcea2d40d6e54299439a01e921f2ff0ce298a2
ItzProbie <142049685+ItzProbie@users.noreply.github.com>
2024-04-17 14:40:07 +0530

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -146,7 +146,7 @@ exports.signUp = async(req,res) => {
                 role,
                 password : hashedPassword,
                 id,
-                image : `https://api.dicebear.com/5.x/initials/svg?seed=${firstName}${lastName}`,
+                image : `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
                 dept
             });
             
